134c5a7ce4eccafb85b29ac71d30142133bcaac8
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-15 13:45:50 -0400

    avances

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+	var nombre = document.getElementsById("name").value;
+	var apellido = document.getElementsById("lastname").value;
+
+}
+
+function mayusculaPrimera(string){
+	return string.charAt(0).toUpperCase();
 }

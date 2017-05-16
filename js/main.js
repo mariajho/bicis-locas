1f2c602b7a00be0aaa812968ed1bcc0da4335c78
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-16 00:02:07 -0400

    actualizar

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@ function validateForm(){
 	/* Escribe tú código aquí */
 	var nombre = document.getElementsById("name").value;
 	var apellido = document.getElementsById("lastname").value;
+	var email = document.getElementsById("input-email").value;
+	var contraseña = document.getElementsById("input-password").value;
+	var seleccion= document.getElementsByTagName("select").value;
+}
+
+nombre();
+
+function nombre(){
+	var nombre = document.getElementsById("name").value;
+	
 
 }
+
+
+
+
 
 function mayusculaPrimera(string){
 	return string.charAt(0).toUpperCase();

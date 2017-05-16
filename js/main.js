7310b1e814dec9d35b43e9b0363c459f8c0c284e
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-16 00:36:26 -0400

    termino

--- js/main.js
+++ js/main.js
@@ -1,24 +1,78 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 	var nombre = document.getElementsById("name").value;
+	alert nombre;
 	var apellido = document.getElementsById("lastname").value;
-	var email = document.getElementsById("input-email").value;
-	var contraseña = document.getElementsById("input-password").value;
+	var correo = document.getElementsById("input-email").value;
+	var password = document.getElementsById("input-password").value;
 	var seleccion= document.getElementsByTagName("select").value;
-}
 
-nombre();
 
 function nombre(){
 	var nombre = document.getElementsById("name").value;
-	
+  if(!(/^[A-Z][a-z]{3,19}$/).test(nombre)){
+  	var crearSpan=document.createElement("span");
+  	var mensaje = document.createTextNode("Nombre inválido, ingrese nuevamente");
+  	var fatherClass=document.getElementsByClassName("name-container input-box")[0];
+    crearSpan.appendChild(mensaje);
+    fatherClass.appendChild(crearSpan);
+    alert nombre;
 
+        }
+    }
+    nombre();
+
+function apellido(){
+	var apellido = document.getElementsById("lastname").value;
+  if(!(/^[A-Z][a-z]{3,19}$/).test(apellido)){
+  	var crearSpan=document.createElement("span");
+  	var mensaje = document.createTextNode("Apellido inválido, ingrese nuevamente");
+  	var fatherClass=document.getElementsByClassName("lastname-container input-box")[0];
+    crearSpan.appendChild(mensaje);
+    fatherClass.appendChild(crearSpan);
+        }
+    }    
+
+    apellido();
+
+       var correo = document.getElementById("input-email").value;
+        if ( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
+            var crearSpan= document.createElement("span");
+            var mensaje= document.createTextNode("Email inválido, ingrese nuevamente");
+            var fatherClass=document.getElementsByClassName("email-container input-box")[0];
+            crearSpan.appendChild(mensaje);
+            fatherClass.appendChild(crearSpan);
+        }
+    }
+    correo();
+
+    function password(){
+        var contrasenia=document.getElementById("input-password").value;
+        if (!(/^(?=.*\d).{6,}$/).test(contrasenia)) {
+            var crearSpan= document.createElement("span");
+            var mensaje= document.createTextNode("Contraseña inválido, ingrese nuevamente");
+            var fatherClass=document.getElementsByClassName("form-group input-box")[0];
+            crearSpan.appendChild(mensaje);
+            fatherClass.appendChild(crearSpan);
+        }
+    }
+    password();
+
+
+     function opciones(){
+    var seleccion=document.getElementsByTagName("select");
+    for (var i=0; i<seleccion.length; i++){
+        if(seleccion[i].value=="0"){
+            var crearSpan= document.createElement("span");
+            var mensaje= document.createTextNode("Debe seleccionar una opción");
+            var fatherClass=document.getElementsByClassName("form-group input-box")[1];
+            crearSpan.appendChild(mensaje);
+            fatherClass.appendChild(crearSpan);
+        }
+    }
+}
+opciones();
 }
 
 
 
-
-
-function mayusculaPrimera(string){
-	return string.charAt(0).toUpperCase();
-}
